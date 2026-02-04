601f64729653d3669ec2cd298de9819dca28f703
Alina <alina.stone@t-online.de>
2026-02-04 15:45:05 +0100

    rechtskonforme Info Bilder und Farbe 4

--- resources/views/livewire/shop/cart.blade.php
+++ resources/views/livewire/shop/cart.blade.php
@@ -296,18 +296,6 @@
                                     <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" /></svg>
                                 </button>
                             </div>
-
-                            {{-- INFO-BOX: Hinweis zur Positionierung --}}
-                            <div class="mx-4 mt-6 sm:mx-6 bg-blue-50 border-l-4 border-blue-400 p-4">
-                                <div class="flex">
-                                    <div class="flex-shrink-0">
-                                        <svg class="h-5 w-5 text-blue-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                            <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd" />
-                                        </svg>
-                                    </div>
-                                </div>
-                            </div>
-
                             {{-- Content --}}
                             <div class="relative flex-1 h-full">
                                 @if($editingItemId)
