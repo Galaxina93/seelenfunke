0c7d3d9b9f513eb4c47c8f698e90b2603f4f5b55
alina <kontakt@mein-seelenfunke.de>
2026-05-01 12:52:37 +0200

    style: increase container width for support telephony and analytics pages

--- resources/views/livewire/shop/support/support-chat-analytics.blade.php
+++ resources/views/livewire/shop/support/support-chat-analytics.blade.php
@@ -10,7 +10,7 @@
     </x-slot>
 
     <div class="py-8">
-        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 space-y-10">
+        <div class="max-w-9xl w-full mx-auto px-4 sm:px-6 lg:px-8 space-y-10">
 
             {{-- 1. Main Summary Cards (Hauptkacheln) --}}
             <div class="grid grid-cols-1 md:grid-cols-3 gap-6 my-4">
--- resources/views/livewire/shop/support/support-telephony.blade.php
+++ resources/views/livewire/shop/support/support-telephony.blade.php
@@ -1,4 +1,4 @@
-<div style="--theme-color: {{ $this->themeColorHex }};" class="p-6 max-w-7xl mx-auto space-y-6">
+<div style="--theme-color: {{ $this->themeColorHex }};" class="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto space-y-6">
     <div class="flex justify-between items-center">
         <div>
             <h1 class="text-3xl font-bold text-white mb-2">
